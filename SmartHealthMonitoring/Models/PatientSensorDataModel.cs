b9fc0c2ef377a75b7fa8768ddc52a49f299641a5
Albiongit <albionademi5@gmail.com>
2024-09-17 20:23:28 +0200

    Patient sensor generate API

---
+++ SmartHealthMonitoring/Models/PatientSensorDataModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartHealthMonitoring.Models;
+
+public class PatientSensorDataModel
+{
+    public Guid SensorNodeId { get; set; }
+    public DateTime TimeStamp { get; set; }
+    public double BodyTemperature { get; set; }
+    public int PulseRate { get; set; }
+    public double RoomTemperature { get; set; }
+    public double RoomHumidity { get; set; }
+}
--- SmartHealthMonitoring/Program.cs
+++ SmartHealthMonitoring/Program.cs
@@ -1,9 +1,14 @@
+using SmartHealthMonitoring.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Register the PatientSensorService as a singleton
+builder.Services.AddSingleton<PatientSensorService>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
