aef8c1a1123ff3edb92e1a1cc9fa551aa065a85b
LirimM <34185066+LirimM@users.noreply.github.com>
2024-09-20 02:32:51 +0200

    Delete SmartHealthMonitoring directory

--- SmartHealthMonitoring/Models/SensorModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SmartHealthMonitoring.Models;
-
-public class SensorModel
-{
-    public string SensorCode { get; set; }
-    public string SensorName { get; set; }
-    public string Manufacturer { get; set; }
-}
