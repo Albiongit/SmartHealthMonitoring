f8ceadc7e04f466835f06ebda9087205528f74c7
LirimM <34185066+LirimM@users.noreply.github.com>
2024-09-20 02:24:52 +0200

    Delete SmartHealthMonitoring directory

--- SmartHealthMonitoring/Models/AggregatedSensorDataModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SmartHealthMonitoring.Models;
-
-public class AggregatedSensorDataModel
-{
-    public SensorModel Sensor { get; set; }
-    public PatientModel Patient { get; set; }
-    public SensorNodeModel SensorNode { get; set; }
-    public List<SensorDataModel> SensorData { get; set; }
-}
